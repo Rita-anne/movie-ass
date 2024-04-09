09f8e6e6408d1ce276aec11c780adbdb88c9fd93
Rita-anne <annegakiza@gmail.com>
2024-04-09 09:20:13 +0200

    okk

--- movie-ass/Welcome.js
+++ movie-ass/Welcome.js
@@ -8,7 +8,7 @@ export default function Welcome({navigation}) {
         <Image  style={{top:50,left:150}} source={require('./assets/movie.jpg')} />
         <Image  style={{top:200,left:150,}} source={require('./assets/back.jpg')} />
         <Text  onPress={()=>navigation.navigate('Src6')} style={{top:210,color:'white',textAlign:'center',fontSize:30,fontWeight:'500' }}>Welcome to Muvi</Text>
-        <Text style={{top:250,color:'white',textAlign:'center',fontSize:25,fontWeight:'100'}} >Free movie streaming all your needs everytime and everywhere.....</Text>
+        <Text style={{top:250,color:'white',textAlign:'center',fontSize:25,fontWeight:'100'}} >Free movie streaming all your needs everytime and everywhere</Text>
         <Pressable onPress={()=>navigation.navigate('Src6')} >
         <Text style={{top:380,textAlign:'center',backgroundColor:'#f9be02',margin:15,padding:5}} > Watch Movie </Text>
        </Pressable>
